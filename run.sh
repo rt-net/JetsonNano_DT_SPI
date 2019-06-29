8265bfef1ef60e5a7e7b36564be27ffcfc0355d1
Tiryoh <tiryoh@gmail.com>
2019-06-29 23:03:32 +0900

    Add MD5 hash check

--- run.sh
+++ run.sh
@@ -9,8 +9,9 @@ if [[ ! $(stat -c '%U' $SRC_DIR/Downloads/Linux_for_Tegra/ 2>/dev/null) == "root
 	mkdir -p $SRC_DIR/backup
 	cd $SRC_DIR/Downloads
 	wget -c https://developer.nvidia.com/embedded/dlc/l4t-jetson-driver-package-32-1-jetson-nano -O Jetson-Nano-Tegra210_Linux_R32.1.0_aarch64.tbz2
-	tar xvf Jetson-Nano-Tegra210_Linux_R32.1.0_aarch64.tbz2
 	wget -c https://developer.nvidia.com/embedded/dlc/l4t-sample-root-filesystem-32-1-jetson-nano -O Jetson-Nano-Tegra_Linux_Sample-Root-Filesystem_R32.1.0_aarch64.tbz2
+	md5sum -c $SRC_DIR/MD5SUM || { echo File download error.; exit 1; }
+	tar xvf Jetson-Nano-Tegra210_Linux_R32.1.0_aarch64.tbz2
 	cd $SRC_DIR/Downloads/Linux_for_Tegra/rootfs/
 	tar xvf $SRC_DIR/Downloads/Jetson-Nano-Tegra_Linux_Sample-Root-Filesystem_R32.1.0_aarch64.tbz2
 	cp $SRC_DIR/Downloads/Linux_for_Tegra/kernel/dtb/tegra210-p3448-0000-p3449-0000-a02.dtb $SRC_DIR/backup/
