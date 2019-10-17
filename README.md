6c54a59a4439e79354becc9b5faf85fbf87faf7b
Tiryoh <tiryoh@gmail.com>
2019-10-17 20:39:44 +0900

    Update install script

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 unofficial script for Jetson Nano(L4T Jetson-Nano-Tegra210_Linux_R32.1.0_aarch64) to enable SPI0 (SPI1 on Developer Kit's Pin 19, 21, 23 and 24)
 
 __Please don't forget to backup your data!__  
-__This script is tested only on L4T R32.1(not tested on > R32.1).  Details: [issue#5](https://github.com/rt-net/JetsonNano_DT_SPI/issues/5).__
+__This script is tested only on L4T R32.2.1(does not work on other version).  Details: [issue#5](https://github.com/rt-net/JetsonNano_DT_SPI/issues/5).__
 
 First, download this repository into your Ubuntu PC (not your Jetson Nano).  
 After downloading, just run:
@@ -33,8 +33,7 @@ The script doesn't depend on the NVIDIA SDK Manager. It downloads the reqirement
 ## Information
 
 The difference between original and modified `tegra210-p3448-0000-p3449-0000-a02.dts`:  
-* https://github.com/rt-net/JetsonNano_DT_SPI/commit/79d4698ac518ab4cc9662ae8bd7e282f56f31cab
-* https://github.com/rt-net/JetsonNano_DT_SPI/commit/0124b15ea6e29a0a45246223b7c892aa39e20678
+* https://github.com/rt-net/JetsonNano_DT_SPI/commit/828c689858098530ac9787da8813c0973b0039c8
 
 SPI1 on J40 of Developer Kit is available from `/dev/spidev0.0`.
 Accroding to "[Jetson Nano Product Design Guide](https://developer.nvidia.com/embedded/dlc/jetson-nano-product-design-guide)" which was published on June 10th, 2019, SPI1(Pin 19, 21, 23 and 24 on Developer Kit) is connected to SPI0 on Tegra X1, not SPI1.  See section "9.2 SPI"(pp. 41).
@@ -47,15 +46,17 @@ https://www.rt-shop.jp/blog/archives/11277
 
 ### Special Thanks
 
-This work was derived from [jas-mx](https://devtalk.nvidia.com/member/3084736/)'s information:
+This work was derived from these works:
 
-https://devtalk.nvidia.com/default/topic/1050427/jetson-nano/enabling-spidev-on-the-jetson-nano-is-hanging-when-flashing/post/5338398/#5338398
+* [jas-mx](https://devtalk.nvidia.com/member/3084736/)'s information:
+  * https://devtalk.nvidia.com/default/topic/1050427/jetson-nano/enabling-spidev-on-the-jetson-nano-is-hanging-when-flashing/post/5338398/#5338398
+* [gtjoseph/jetson-nano-support](https://github.com/gtjoseph/jetson-nano-support/tree/l4t_32.2.1)
 
-### tegra210-p3448-0002-p3449-0000-a02.dts
+### tegra210-p3448-0000-p3449-0000-a02.dts
 
 ```
 /*
- * arch/arm64/boot/dts/tegra210-p3448-0002-p3449-0000-a02.dts
+ * arch/arm64/boot/dts/tegra210-p3448-0000-p3449-0000-a02.dts
  *
  * Copyright (c) 2018-2019, NVIDIA CORPORATION.  All rights reserved.
  *
--- run.sh
+++ run.sh
@@ -8,21 +8,28 @@ if [[ ! $(stat -c '%U' $SRC_DIR/Downloads/Linux_for_Tegra/ 2>/dev/null) == "root
 	mkdir -p $SRC_DIR/Downloads/Linux_for_Tegra
 	mkdir -p $SRC_DIR/backup
 	cd $SRC_DIR/Downloads
-	wget -c https://developer.nvidia.com/embedded/dlc/l4t-jetson-driver-package-32-1-jetson-nano -O Jetson-Nano-Tegra210_Linux_R32.1.0_aarch64.tbz2
-	wget -c https://developer.nvidia.com/embedded/dlc/l4t-sample-root-filesystem-32-1-jetson-nano -O Jetson-Nano-Tegra_Linux_Sample-Root-Filesystem_R32.1.0_aarch64.tbz2
+	wget -c https://developer.nvidia.com/embedded/dlc/Jetson-210_Linux_R32.2.1 -O Jetson-Nano-Tegra210_Linux_R32.2.1_aarch64.tbz2
+	wget -c https://developer.nvidia.com/embedded/dlc/r32-2-1_Release_v1.0/Nano-TX1/Tegra_Linux_Sample-Root-Filesystem_R32.2.1_aarch64.tbz2 -O Jetson-Nano-Tegra_Linux_Sample-Root-Filesystem_R32.2.1_aarch64.tbz2
 	md5sum -c $SRC_DIR/MD5SUM || { echo File download error.; exit 1; }
-	tar xvf Jetson-Nano-Tegra210_Linux_R32.1.0_aarch64.tbz2
+	tar xvf Jetson-Nano-Tegra210_Linux_R32.2.1_aarch64.tbz2
 	cd $SRC_DIR/Downloads/Linux_for_Tegra/rootfs/
-	tar xvf $SRC_DIR/Downloads/Jetson-Nano-Tegra_Linux_Sample-Root-Filesystem_R32.1.0_aarch64.tbz2
+	tar xvf $SRC_DIR/Downloads/Jetson-Nano-Tegra_Linux_Sample-Root-Filesystem_R32.2.1_aarch64.tbz2
+	cd $SRC_DIR/Downloads/Linux_for_Tegra/
 	cp $SRC_DIR/Downloads/Linux_for_Tegra/kernel/dtb/tegra210-p3448-0000-p3449-0000-a02.dtb $SRC_DIR/backup/
 	dtc -I dtb -O dts -o $SRC_DIR/backup/tegra210-p3448-0000-p3449-0000-a02.dts $SRC_DIR/backup/tegra210-p3448-0000-p3449-0000-a02.dtb
-	grep -q 'nvidia,dtbbuildtime = "Mar 13 2019", "00:37:56"' $SRC_DIR/backup/tegra210-p3448-0000-p3449-0000-a02.dts && echo Device-Tree version mach || { echo Device-Tree version miss-mach; exit 1; }
+	grep -q 'nvidia,dtbbuildtime = "Aug 12 2019", "21:21:24"' $SRC_DIR/backup/tegra210-p3448-0000-p3449-0000-a02.dts && echo Device-Tree version mach || { echo Device-Tree version miss-mach; exit 1; }
 	dtc -I dts -O dtb -o $SRC_DIR/tegra210-p3448-0000-p3449-0000-a02.dtb $SRC_DIR/tegra210-p3448-0000-p3449-0000-a02.dts
 	cp $SRC_DIR/tegra210-p3448-0000-p3449-0000-a02.dtb $SRC_DIR/Downloads/Linux_for_Tegra/kernel/dtb/tegra210-p3448-0000-p3449-0000-a02.dtb
+	wget -c https://github.com/Tiryoh/tegra_u-boot/releases/download/tegra-l4t-r32.2.1-spi1/u-boot.bin -O $SRC_DIR/Downloads/u-boot.bin
+	cp $SRC_DIR/Downloads/u-boot.bin $SRC_DIR/Downloads/Linux_for_Tegra/bootloader/t210ref/p3450-porg/u-boot.bin
+	wget -c https://raw.githubusercontent.com/gtjoseph/jetson-nano-support/585d634cf5c989242460352640c0c462e7be9e3f/flash/flash-partitions.sh -O $SRC_DIR/Downloads/Linux_for_Tegra/flash-partitions.sh
+	chmod +x $SRC_DIR/Downloads/Linux_for_Tegra/flash-partitions.sh
+	wget -c https://raw.githubusercontent.com/gtjoseph/jetson-nano-support/585d634cf5c989242460352640c0c462e7be9e3f/flash/flash2.sh -O $SRC_DIR/Downloads/Linux_for_Tegra/flash2.sh
+	chmod +x $SRC_DIR/Downloads/Linux_for_Tegra/flash2.sh
 	sudo chown root:root -R $SRC_DIR/Downloads/Linux_for_Tegra
 fi
 
 read -n1 -p "Turn your Jetson Nano into Recovery Mode. Are you ready?[y/n] " yn; [[ $yn = [yY] ]] || { echo abort.; exit 1; }
 cd $SRC_DIR/Downloads/Linux_for_Tegra/
 sudo ./apply_binaries.sh
-sudo ./flash.sh --no-systemimg -k DTB jetson-nano-qspi-sd mmcblk0p1
+sudo ./flash-partitions.sh --partitions=EBT,RP1,LNX,DTB jetson-nano-qspi-sd mmcblk0p1
